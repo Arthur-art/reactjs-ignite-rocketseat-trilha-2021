221ccf003c1a93373c5fc23398a1cbdac4568ec0
Arthur-art <arthurteixeira.guts@gmail.com>
2021-07-15 17:38:00 -0300

    Criando estilos globais

--- Chapter-02/my-app/src/App.tsx
+++ Chapter-02/my-app/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import styled from 'styled-components'
+import { GlobalStyle } from './styles/global';
 
 const Title = styled.h1`
   color: black;
-`
+`;
 
 export function App() {
   return (
     <div className="App">
       <Title>Teste</Title>
+      <GlobalStyle />
     </div>
   );
 }

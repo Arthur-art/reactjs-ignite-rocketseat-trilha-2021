6cc39bf7483bb0981e0ee75fda2bb7bda854dd4e
Arthur-art <arthurteixeira.guts@gmail.com>
2021-07-20 11:25:19 -0300

    Salvando dados dos inputs atraves do parametro event nos states do react

--- Chapter-02/dev-finance-react/src/components/NewTransactionModal/index.tsx
+++ Chapter-02/dev-finance-react/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FormEvent } from 'react';
 import Modal from 'react-modal';
 import { Container, TransactionTypeContainer, RadioBox } from './style';
 import closeImg from "../../assets/close.svg"
@@ -8,7 +8,21 @@ import outcomeImg from "../../assets/outcome.svg"
 
 export const NewTransactionModal = ({ modalTransaction, afterOpenModal, handleModalClose, }) => {
 
-    const [type, setType] = useState("deposit")
+    const [type, setType] = useState("deposit");
+
+    const [title, setTitle] = useState("")
+    const [valueMoney, setValueMoney] = useState(0)
+    const [category, setCategory] = useState("")
+
+
+    const handleCreateNewTransaction = (event: FormEvent) => {
+        event.preventDefault()
+        console.log(
+            title,
+            valueMoney,
+            category
+        )
+    }
 
     return (
         <div>
@@ -22,12 +36,12 @@ export const NewTransactionModal = ({ modalTransaction, afterOpenModal, handleMo
                 <button className="button-close-modal" onClick={handleModalClose}>
                     <img src={closeImg} alt="Fechar modal" />
                 </button>
-                <Container action="#">
+                <Container onSubmit={handleCreateNewTransaction}>
                     <h2>Cadastrar transação</h2>
 
-                    <input type="text" placeholder="Titulo" />
+                    <input onChange={(event) => setTitle(event.target.value)} value={title} type="text" placeholder="Titulo" />
 
-                    <input type="number" placeholder="Valor" />
+                    <input onChange={(event) => setValueMoney(Number(event.target.value))} value={valueMoney} type="number" placeholder="Valor" />
 
                     <TransactionTypeContainer>
                         <RadioBox activeColor={"green"} active={type === 'deposit'} onClick={() => setType("deposit")}>
@@ -40,7 +54,7 @@ export const NewTransactionModal = ({ modalTransaction, afterOpenModal, handleMo
                         </RadioBox>
                     </TransactionTypeContainer>
 
-                    <input type="text" placeholder="Categoria" />
+                    <input onChange={(event) => setCategory(event.target.value)} value={category} type="text" placeholder="Categoria" />
 
                     <button type="submit">Cadastrar</button>
                 </Container>

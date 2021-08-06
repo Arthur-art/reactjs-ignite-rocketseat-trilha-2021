4efc8085c915e6546f4a991bef2240c6610ccdc4
Arthur-art <arthurteixeira.guts@gmail.com>
2021-08-06 13:59:02 -0300

    Desenvolvendo component SubscribeButton

--- Chapter-03-NextJS/ignews/src/pages/index.tsx
+++ Chapter-03-NextJS/ignews/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { SubscribeButton } from "../components/SubscribeButton"
 import styles from "./home.module.scss"
 
 export default function Home() {
@@ -12,9 +13,10 @@ export default function Home() {
           <span>Hey welcome!</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>
-            Get access to all the publications <br/>
+            Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>

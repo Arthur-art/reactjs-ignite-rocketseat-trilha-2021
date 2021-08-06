23ba9849b8676b20b4e2778267da2c131afad099
Arthur-art <arthurteixeira.guts@gmail.com>
2021-08-06 15:47:20 -0300

    Consumindo api com Static (SSG)

--- Chapter-03-NextJS/ignews/src/pages/index.tsx
+++ Chapter-03-NextJS/ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Head from "next/head"
 import { SubscribeButton } from "../components/SubscribeButton"
 import { stripe } from "../service/stripe"
@@ -26,7 +26,7 @@ export default function Home({ product }: HomeProps) {
             Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId}/>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
@@ -34,7 +34,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   // Id do preço
   const price = await stripe.prices.retrieve("price_1JLWW4HvuE7HOtwv0D29ZgPn", {
@@ -52,6 +52,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
   }
 }

c8ca7d91d625c1b96baf6092e22b662492976970
Arthur-art <arthurteixeira.guts@gmail.com>
2021-08-09 17:58:24 -0300

    Salvando primeiro usuario no banco de dados do faunadb

--- Chapter-03-NextJS/ignews/src/pages/api/auth/[...nextauth].ts
+++ Chapter-03-NextJS/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { fauna } from '../../../service/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,7 +9,28 @@ export default NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope:"read:user"
+      scope: "read:user"
     }),
   ],
+  jwt:{
+    signingKey: process.env.SIGNING_KEY
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true;
+      } catch (error) {
+        return false
+      }
+    }
+  }
 })

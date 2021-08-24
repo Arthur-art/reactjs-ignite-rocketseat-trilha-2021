73c9a687dd714e2d5548a15d38dee1582efaf54a
arthur-art <arthurteixeira.guts@gmail.com>
2021-08-24 16:47:43 -0300

    Adicionando grafico do apexcharts, estilizando grafico e adicionando valores

--- Chapter-04-PainelAdm/dashboard/src/pages/dashboard.tsx
+++ Chapter-04-PainelAdm/dashboard/src/pages/dashboard.tsx
@@ -1,6 +1,64 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+import dynamic from 'next/dynamic'
+import { ApexOptions } from 'apexcharts';
+const Chart = dynamic(() => import('react-apexcharts'), {
+    ssr: false
+});
+
+const options: ApexOptions = {
+    chart: {
+        //Removendo o menu dasboard
+        toolbar: {
+            show: false,
+        },
+        //Retirando zoom por default
+        zoom: {
+            enabled: false,
+        },
+        //Alterando cores dos textos do grafico
+        foreColor: theme.colors.gray[500]
+    },
+    grid: {
+        show: false
+    },
+    dataLabels: {
+        enabled: false
+    },
+    tooltip: {
+        enabled: false
+    },
+    xaxis: {
+        type: "datetime",
+        axisBorder: {
+            color: theme.colors.gray[600]
+        },
+        axisTicks: {
+            color: theme.colors.gray[600]
+        },
+        //Para cada dado no array data: de series é preciso passar uma categoria, neste caso datas
+        categories: [
+
+            "2021-08-24T00:00.00Z",
+            "2021-08-23T00:00.00Z",
+            "2021-08-22T00:00.00Z",
+            "2021-08-21T00:00.00Z",
+            "2021-08-20T00:00.00Z",
+            "2021-08-19T00:00.00Z",
+            "2021-08-18T00:00.00Z"
+        ]
+
+    },
+    //Estilizando grafico
+    fill: {
+        opacity: 0.3
+    }
+};
+
+const series = [
+    { name: "series1", data: [122, 123, 12, 300, 25, 24, 109] }
+];
 
 export default function Dashboard() {
 
@@ -11,6 +69,19 @@ export default function Dashboard() {
 
                 <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                     <Sidebar />
+                    {/**SimpleGrid, component do chakra.ui para habilita uso de css grid
+                     * minChildWidth: Todos os itens dentro do grid devem ter no minimo 320px                                                  
+                     */}
+                    <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+                        <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+                            <Text fontSize="lg" mb="4">Inscritos das semana</Text>
+                            <Chart options={options} series={series} type="area" height={160} />
+                        </Box>
+                        <Box p="8" bg="gray.800" borderRadius={8}>
+                            <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+                            <Chart options={options} series={series} type="area" height={160} />
+                        </Box>
+                    </SimpleGrid>
                 </Flex>
             </Flex>
         </>
--- Chapter-04-PainelAdm/doc.md
+++ Chapter-04-PainelAdm/doc.md
@@ -10,3 +10,19 @@
 
 # Trabalhando com lib react-icons
 - yarn add react-icons
+
+# Trabalhando com lib apexcharts para geração de gráficos
+- https://apexcharts.com/
+- yarn add apexcharts react-apexcharts
+
+# Resolvendo erro de window is not defined gerado pelo next ao tentar reenderizar o apex-charts
+- Esse erro ocorre pois o primeiro carregamento do next é feito no servidor back-end que roda dentro de um processo Node, e nesse processo não existe o window que é utilizado pelo apex-charts
+
+para resolver é bem simples, é só importar o Dynamic 
+
+```js
+import dynamic from 'next/dynamic'
+const Chart = dynamic(() => import('react-apexcharts'), {
+    ssr: false
+});
+```

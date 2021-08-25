c380ac177f89b171eaf187a13f06832c3cbfa610
arthur-art <arthurteixeira.guts@gmail.com>
2021-08-25 16:15:16 -0300

    Desenvolvendo components de partes do Pagination

---
+++ Chapter-04-PainelAdm/dashboard/src/components/Pagination/PaginationItem.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@chakra-ui/react";
+
+interface PaginationItemProps {
+    number: number;
+    isCurrent?: boolean;
+}
+
+export function PaginationItem({ isCurrent = false, number }: PaginationItemProps) {
+
+    if (isCurrent) {
+
+        return (
+            <Button size="sm" fontSize="xs" width="4" colorScheme="blackAlpha" disabled _disabled={{
+                cursor: "default"
+            }}>
+                {number}
+            </Button>
+        )
+    }
+
+    return (
+        <Button size="sm" fontSize="xs" width="4" colorScheme="blackAlpha" _hover={{
+            bg: "gray.500"
+        }}>
+            {number}
+        </Button>
+    )
+
+
+}

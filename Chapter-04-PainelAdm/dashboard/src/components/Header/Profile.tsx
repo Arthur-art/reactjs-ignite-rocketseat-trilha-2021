eca3bc64a954d7bf6d023e571fc139bfc9e85fff
arthur-art <arthurteixeira.guts@gmail.com>
2021-08-25 17:54:21 -0300

    Trabalhando com hook do chakra useBreakpointValue

--- Chapter-04-PainelAdm/dashboard/src/components/Header/Profile.tsx
+++ Chapter-04-PainelAdm/dashboard/src/components/Header/Profile.tsx
@@ -1,14 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react"
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
     return (
         <>
             {/**Dados do perfil */}
             <Flex align="center" >
-                <Box mr="4" textAlign="right">
+                {showProfileData && <Box mr="4" textAlign="right">
                     <Text>Arthur Teixeira S. Silva</Text>
                     <Text color="gray.300" as="small">arthurteixeira.guts@gmail.com</Text>
-                </Box>
+                </Box>}
                 <Avatar bgColor="gray.300" size="md" name="Arthur Teixeira S. Silva"
                     src="https://avatars.githubusercontent.com/u/54858003?v=4" />
             </Flex >
--- Chapter-04-PainelAdm/dashboard/src/components/Header/index.tsx
+++ Chapter-04-PainelAdm/dashboard/src/components/Header/index.tsx
@@ -1,10 +1,19 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from "./Logo";
 import { Notification } from "./Notification";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+
+    /**Trabalhando com hook useBreakpointValue do chakra.ui para responder de acordo com o redimensionamento da tela,
+     * a constante isWideVersion recebe true se o tamanho da tela for lg ou maior, se a tela diminuir demais abaixo de lg  
+     * ela recebe false 
+     */
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    })
 
     return (
         <Flex as="header" w="100%" maxW={1480} h="20" mx="4" px="6" align="center" >
@@ -15,7 +24,7 @@ export function Header() {
 
             <Notification />
 
-            <Profile />
+            <Profile showProfileData={isWideVersion} />
 
         </Flex>
     )

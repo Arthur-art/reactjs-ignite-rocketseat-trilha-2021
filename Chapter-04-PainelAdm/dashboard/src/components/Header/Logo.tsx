28aebf9a28d08e180313c8d420536f8c9eadc07c
arthur-art <arthurteixeira.guts@gmail.com>
2021-08-25 17:05:45 -0300

    Desenvolvendo responsividade no component Logo

--- Chapter-04-PainelAdm/dashboard/src/components/Header/Logo.tsx
+++ Chapter-04-PainelAdm/dashboard/src/components/Header/Logo.tsx
@@ -2,7 +2,8 @@ import { Text } from "@chakra-ui/react";
 
 export function Logo() {
     return (
-        <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+        //Cada valor dentro do array de fontsize representa uma resolução
+        <Text fontSize={["2xl", "3xl", "4xl"]} fontWeight="bold" letterSpacing="tight" w="64">
             Dashboard
             <Text as="span" color="red.400">.</Text>
         </Text>

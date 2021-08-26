39dd49130a54b532cff4187b4456482a74554a0f
arthur-art <arthurteixeira.guts@gmail.com>
2021-08-26 16:44:53 -0300

    Desenvolvendo form de autentificacao de login

--- Chapter-04-PainelAdm/dashboard/src/pages/index.tsx
+++ Chapter-04-PainelAdm/dashboard/src/pages/index.tsx
@@ -1,21 +1,30 @@
 import { Button, Flex, Stack } from "@chakra-ui/react"
 import { InputComponent } from "../components/Form/InputComponent"
-import { useForm } from "react-hook-form"
+import { SubmitHandler, useForm } from "react-hook-form"
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
 
 
 export default function SigIn() {
 
-  const { register, handleSubmit } = useForm()
+  /**O metodo handleSubmit recebe os valores do register e esses valores podem ser recuperados
+   * atraves de um parametro value dentro da function que o handleSubmit do useForm está envolvendo
+   */
+  const { register, handleSubmit, formState } = useForm()
 
-  function handleSignIn() {
-
+  const handleSignIn: SubmitHandler<SignInFormData> = async (value) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    console.log(value)
   }
 
   return (
     // Utilizando component Flex do chakra, que funciona como uma div com display:flex por default
     <Flex w={"100vw"} h={"100vh"} alignItems={"center"} justifyContent={"center"}>
       {/**Todo component Flex por padrão é uma div, basta passar por props as="form" para transformalo em forms */}
-      <Flex as="form" flexDirection={"column"} maxWidth={360} width={"100%"} bg={"gray.800"} p={"8"} borderRadius={"8"}>
+      <Flex onSubmit={handleSubmit(handleSignIn)} as="form" flexDirection={"column"} maxWidth={360} width={"100%"} bg={"gray.800"} p={"8"} borderRadius={"8"}>
         {/**Component Stack para adicionar espaçamentos nos elementos */}
         <Stack spacing="4">
 
@@ -24,7 +33,8 @@ export default function SigIn() {
           <InputComponent {...register("password")} name={"password"} type="password" label={"Password"} />
 
         </Stack>
-        <Button type="submit" mt="6" colorScheme="blackAlpha">Entrar</Button>
+        {/**formState.isSubmitting observa o estado do forms e retorna um booleano */}
+        <Button isLoading={formState.isSubmitting} type="submit" mt="6" colorScheme="blackAlpha">Entrar</Button>
       </Flex>
     </Flex>
   )
